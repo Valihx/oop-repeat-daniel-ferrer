a18dbd26dc2ddb7fff276e391d79d944eb00e1bb
Valihx <daniel.ferrerb12@gmail.com>
2025-08-23 21:25:26 +0100

    made the mainapp, just gave me error cause the methods are still null

---
+++ src/com/musicshop/client/mainApp.java
@@ -0,0 +1,25 @@
+package com.musicshop.client;
+
+import com.musicshop.dao.InstrumentDaoImplementation;
+import com.musicshop.dao.InstrumentDaoInterface;
+import com.musicshop.dto.Instrument;
+import java.util.List;
+
+public class mainApp {
+    public static void main(String[] args) {
+        InstrumentDaoInterface instrumentDao = new InstrumentDaoImplementation();
+
+        System.out.println("Making sure all instruments are on the list");
+
+        List<Instrument> instruments = instrumentDao.getAllInstruments();
+
+        if(instruments.isEmpty()){
+            System.out.println("Error. The instrument list is empty");
+        }
+        else{
+            System.out.println("All instruments are on the list");
+        }
+
+
+    }
+}
