79ab7c98711932d5ea107c0070c2057c7d90a355
Valihx <daniel.ferrerb12@gmail.com>
2025-08-23 21:11:47 +0100

    implementation done

--- src/com/musicshop/dao/InstrumentDaoImplementation.java
+++ src/com/musicshop/dao/InstrumentDaoImplementation.java
@@ -1,5 +1,41 @@
 package com.musicshop.dao;
  import com.musicshop.dto.Instrument;
  import java.util.List;
+
 public class InstrumentDaoImplementation implements InstrumentDaoInterface {
+
+    @Override
+    public List<Instrument> getAllInstruments() {
+        return null;
+    }
+
+    @Override
+    public Instrument getInstrumentById(int instrumentId) {
+        return null;
+    }
+
+    @Override
+    public void deleteInstrumentById(int instrumentId) {
+    }
+
+    @Override
+    public Instrument insertInstrument(Instrument instrument) {
+        return null;
+    }
+
+    @Override
+    public List<Instrument> getInstrumentsByType(String instrumentName) {
+        return List.of();
+    }
+
+    @Override
+    public String getAllInstrumentsAsJson() {
+        return "";
+    }
+
+    @Override
+    public String getInstrumentByIdAsJson(int instrumentId) {
+        return "";
+    }
+
 }
