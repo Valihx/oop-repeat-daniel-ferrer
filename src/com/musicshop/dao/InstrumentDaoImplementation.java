3c23cf9b80cfc17ba249f3b2bf6c67e0ee422265
Valihx <daniel.ferrerb12@gmail.com>
2025-08-23 21:06:46 +0100

    interface is finished, now the implementation

---
+++ src/com/musicshop/dao/InstrumentDaoImplementation.java
@@ -0,0 +1,5 @@
+package com.musicshop.dao;
+ import com.musicshop.dto.Instrument;
+ import java.util.List;
+public class InstrumentDaoImplementation implements InstrumentDaoInterface {
+}
--- src/com/musicshop/dao/InstrumentDaoInterface.java
+++ src/com/musicshop/dao/InstrumentDaoInterface.java
@@ -3,6 +3,32 @@ package com.musicshop.dao;
 import com.musicshop.dto.Instrument;
 import java.util.List;
 
-public class InstrumentDaoInterface {
+public interface InstrumentDaoInterface {
+    //features
+
+    //find entities
+    public List<Instrument> getAllInstruments();
+
+    //find and display entity by key
+    public Instrument getInstrumentById(int instrumentId);
+
+    //delete by key
+    public void deleteInstrumentById(int instrumentId);
+
+    //insert
+    public Instrument insertInstrument(Instrument instrument);
+
+    //list by filter
+    public List<Instrument> getInstrumentsByType(String instrumentName);
+
+    //create cache using hashset gets added later onnn
+
+    //retrieve  as JSON strong
+    public String getAllInstrumentsAsJson();
+
+    //find single entity by key
+    public String getInstrumentByIdAsJson(int instrumentId);
+
+
 
 }
