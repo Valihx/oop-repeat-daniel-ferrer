c4ae42e7d6693a98539dc04f81eb8eff5ed8e8f9
Valihx <daniel.ferrerb12@gmail.com>
2025-09-07 22:47:29 +0100

    implementsd exception to main

--- src/com/musicshop/dao/InstrumentDaoInterface.java
+++ src/com/musicshop/dao/InstrumentDaoInterface.java
@@ -1,34 +1,32 @@
 package com.musicshop.dao;
 
-import com.musicshop.exception.DaoException;
 import com.musicshop.dto.Instrument;
+import com.musicshop.exception.DaoException;
 import java.util.List;
 
 public interface InstrumentDaoInterface {
     //features
 
     //find entities
-    public List<Instrument> getAllInstruments();
+    public List<Instrument> getAllInstruments() throws DaoException;
 
     //find and display entity by key
-    public Instrument getInstrumentById(int instrumentId);
+    public Instrument getInstrumentById(int instrumentId) throws DaoException;
 
     //delete by key
-    public void deleteInstrumentById(int instrumentId);
+    public void deleteInstrumentById(int instrumentId) throws DaoException;
 
     //insert
-    public Instrument insertInstrument(Instrument instrument);
+    public Instrument insertInstrument(Instrument instrument) throws DaoException;
 
     //list by filter
-    public List<Instrument> getInstrumentsByType(String instrumentName);
+    public List<Instrument> getInstrumentsByType(String instrumentType) throws DaoException;
 
     //create cache using hashset gets added later onnn
 
-    //retrieve  as JSON strong
-    public String getAllInstrumentsAsJson();
+    //retrieve all entities as a JSON string
+    public String getAllInstrumentsAsJson() throws DaoException;
 
-    //find single entity by key
-    public String getInstrumentByIdAsJson(int instrumentId);
-
-
+    //find single entity by key as a JSON string
+    public String getInstrumentByIdAsJson(int instrumentId) throws DaoException;
 }
