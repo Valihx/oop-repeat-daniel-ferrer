3c66b1adcea092a6bd4daddd68e43a12a43d61fe
Valihx <daniel.ferrerb12@gmail.com>
2025-09-08 01:23:51 +0100

    did the 4 "most important" features in the dao

--- src/com/musicshop/dao/InstrumentDaoImplementation.java
+++ src/com/musicshop/dao/InstrumentDaoImplementation.java
@@ -1,11 +1,11 @@
 package com.musicshop.dao;
 
- import com.musicshop.dto.Instrument;
- import com.musicshop.exception.DaoException;
- import com.musicshop.util.DBConnectionManager;
- import java.sql.*;
- import java.util.ArrayList;
- import java.util.List;
+import com.musicshop.dto.Instrument;
+import com.musicshop.exception.DaoException;
+import com.musicshop.util.DBConnectionManager;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InstrumentDaoImplementation implements InstrumentDaoInterface {
 
@@ -13,69 +13,102 @@ public class InstrumentDaoImplementation implements InstrumentDaoInterface {
     public List<Instrument> getAllInstruments() throws DaoException {
         List<Instrument> instruments = new ArrayList<Instrument>();
         //make db connection
-        try(Connection conn = DBConnectionManager.getConnection();
-            PreparedStatement psm = conn.prepareStatement("SELECT * FROM Instrument");
-            ResultSet rs = psm.executeQuery();){
+        try (Connection conn = DBConnectionManager.getConnection();
+             PreparedStatement psm = conn.prepareStatement("SELECT * FROM instruments");
+             ResultSet rs = psm.executeQuery();) {
             //get stuff
-            while(rs.next()){
-                int  id = rs.getInt("id");
+            while (rs.next()) {
+                int id = rs.getInt("id");
                 String name = rs.getString("name");
                 String type = rs.getString("type");
                 double price = rs.getDouble("price");
+                int stock = rs.getInt("stock");
                 //add to the arraylist
-                instruments.add(new Instrument(id,name,type,price));
+                instruments.add(new Instrument(id, name, type, price, stock));
             }
         } catch (SQLException e) {
             throw new DaoException("getAllInstruments failed: " + e.getMessage());
         }
-            return instruments;
+        return instruments;
     }
 
     @Override
     public Instrument getInstrumentById(int instrumentId) throws DaoException {
         Instrument instrument = null;
-        try(Connection conn = DBConnectionManager.getConnection();
-            PreparedStatement psm = conn.prepareStatement("SELECT * FROM instruments WHERE instrument_id = ?");)
-        {
+        try (Connection conn = DBConnectionManager.getConnection();
+             PreparedStatement psm = conn.prepareStatement("SELECT * FROM instruments WHERE id = ?");) {
             psm.setInt(1, instrumentId);
             ResultSet rs = psm.executeQuery();
-            if(rs.next()){
+
+            //check if an instrument was found
+            if (rs.next()) {
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
                 String type = rs.getString("type");
                 double price = rs.getDouble("price");
+                int stock = rs.getInt("stock");
 
-                instrument = new Instrument(id,name,type,price);
+                instrument = new Instrument(id, name, type, price, stock);
             }
 
-
-        } catch (SQLException e){
+        } catch (SQLException e) {
             throw new DaoException("getInstrumentById failed: " + e.getMessage());
         }
         return instrument;
     }
 
     @Override
-    public void deleteInstrumentById(int instrumentId) {
+    public void deleteInstrumentById(int instrumentId) throws DaoException {
+        try (Connection conn = DBConnectionManager.getConnection();
+             PreparedStatement psm = conn.prepareStatement("DELETE FROM instruments WHERE id = ?")) {
+
+            psm.setInt(1, instrumentId);
+            //use executeUpdate to update the db
+            psm.executeUpdate();
+
+            System.out.println("Instrument" + instrumentId + " has been deleted");
+
+        } catch (SQLException e) {
+            throw new DaoException("deleteInstrumentById failed: " + e.getMessage());
+        }
     }
 
     @Override
-    public Instrument insertInstrument(Instrument instrument) {
-        return null;
+    public Instrument insertInstrument(Instrument instrument) throws DaoException {
+        try (Connection conn = DBConnectionManager.getConnection();
+             PreparedStatement psm = conn.prepareStatement("INSERT INTO instruments (name, type, price, stock) VALUES (?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS)) {
+
+            psm.setString(1, instrument.getName());
+            psm.setString(2, instrument.getType());
+            psm.setDouble(3, instrument.getPrice());
+            psm.setInt(4, instrument.getStock());
+
+            psm.executeUpdate();
+
+            // get the new id the database made
+            ResultSet generatedKeys = psm.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                instrument.setInstrumentId(generatedKeys.getInt(1));
+            }
+
+        } catch (SQLException e) {
+            throw new DaoException("insertInstrument failed: " + e.getMessage());
+        }
+        return instrument;
     }
 
     @Override
-    public List<Instrument> getInstrumentsByType(String instrumentName) {
+    public List<Instrument> getInstrumentsByType(String instrumentName) throws DaoException {
         return List.of();
     }
 
     @Override
-    public String getAllInstrumentsAsJson() {
+    public String getAllInstrumentsAsJson() throws DaoException {
         return "";
     }
 
     @Override
-    public String getInstrumentByIdAsJson(int instrumentId) {
+    public String getInstrumentByIdAsJson(int instrumentId) throws DaoException {
         return "";
     }
 
